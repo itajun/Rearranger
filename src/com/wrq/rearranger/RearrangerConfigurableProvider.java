02205704e376c0ed3d872ba04e886b37ea721e3f
Itamar Vieira Junior <itamar.vieira@nuix.com>
2017-09-03 08:07:02 +1000

    Workaround to display the settings panel in idea >=2016. Some work still required to adapt to the new API (mainly settings persistence, but also other deprecated classes), but this worked with minimum impact. IDEA >=2016 requires a new entry for config in the plugin.xml, so I simply created a provider that returns the component to mimic the behaviour of previous versions.
        <extensions defaultExtensionNs="com.intellij">
            <projectConfigurable provider="com.wrq.rearranger.RearrangerConfigurableProvider"/>
        </extensions>

--- src/com/wrq/rearranger/RearrangerConfigurableProvider.java
+++ src/com/wrq/rearranger/RearrangerConfigurableProvider.java
@@ -7,6 +7,18 @@ import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurableProvider;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * <p>
+ * IDEA >=2016 requires configuration panes to be specified in the {@code extensions} tag of the
+ * plugin.xml file. To avoid massive changes in the architecture and to keep the plugin compatible
+ * with previous versions, the config panel points to this provider, which simply finds the
+ * current instance of the plugin and returns it in {@link #createConfigurable()}.
+ * </p>
+ * <p>
+ * Note: Pointing directly to {@link Rearranger} as a {@code Configurable} wouldn't be an option
+ * because we'd have two instances of it with different settings.
+ * </p>
+ */
 public class RearrangerConfigurableProvider extends ConfigurableProvider
 {
     @Nullable
